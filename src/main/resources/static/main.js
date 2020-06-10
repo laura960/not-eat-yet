5ed2f292c56a604397d9b1922196545f2ec66062
Luis Pardus <luis.pardus@gmail.com>
2020-06-10 12:08:42 +0200

    grafica modal login rev01

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -510,7 +510,34 @@ $(document).ready(function(){
 //}
 ///*fine modal 02*/
 
+		/* Modal login */
+		// Get the modal
+		var modal = document.getElementById("modaleLogin");
 
+		// Get the button that opens the modal
+		var btn = document.getElementById("modale-login");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		btn.onclick = function() {
+		modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+		}
+
+		/* fine modal login */
 
 })
 
