91f0d2d3a398ea1ecfae643ea5a8d0cc9d953fef
Laura <laura.vespa96@gmail.com>
2020-06-12 14:18:53 +0200

    security conf

---
+++ src/main/java/org/generation/italy/progettofinaleDemo/controller/AccountController.java
@@ -0,0 +1,16 @@
+package org.generation.italy.progettofinaleDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accountmanager")
+public class AccountController {
+	
+	@GetMapping
+	// @PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String get() {
+		return "<h1>Welcome account manager</h1>";
+	}
+}
