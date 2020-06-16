fbfe7d3e674e1d60a7c12eb343c9dc544f519daa
Laura <laura.vespa96@gmail.com>
2020-06-16 14:31:51 +0200

    rev 01

--- src/main/java/org/generation/italy/progettofinaleDemo/controller/SignupController.java
+++ src/main/java/org/generation/italy/progettofinaleDemo/controller/SignupController.java
@@ -17,65 +17,54 @@ public class SignupController {
 	@PostMapping
 	public String signup(@RequestParam String email, @RequestParam String username, @RequestParam String password, @RequestParam String ruolo) {
 		authService.signup(email, username, password, ruolo);
-		return "<!DOCTYPE html>\n" + 
-				"<html lang=\"en\">\n" + 
-				"<head>\n" + 
-				"<meta charset=\"UTF-8\">\n" + 
-				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + 
-				"<title>NotEatYet</title>\n" + 
-				"<script src=\"/js/jquery-3.5.1.min.js\"></script>\n" + 
-				"<script src=\"/js/main2.js\"></script>\n" + 
-				"<link rel=\"stylesheet\" href=\"/css/style.css\">\n" + 
-				"\n" + 
-				"<!-- jQuery Modal -->\n" + 
-				"<script\n" + 
-				"	src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js\"></script>\n" + 
-				"\n" + 
-				"</head>\n" + 
-				"<body>\n" + 
-				"	<header class=\"top-header\">\r\n" + 
-				"		<nav class=\"top-nav\">\r\n" + 
-				"\r\n" + 
-				"			<li><input id=\"modal-registrati\" class=\"registrati button\"\r\n" + 
-				"					type=\"button\" value=\"Registrati\"></li>\r\n" + 
-				"\r\n" + 
-				"			<!--LOGO-->\r\n" + 
-				"			<li class=\"logo\"><a href=\"/index.html\"><img class=\"img-logo\"\r\n" + 
-				"					src=\"https://i.ibb.co/RDLFZ4c/prova06.png\" alt=\"\"></a></li>\r\n" + 
-				"\r\n" + 
-				"			<!--REGISTRA LOGIN-->\r\n" + 
-				"			<div class=\"top-reg-log\">\r\n" + 
-				"				<li><input id=\"modal-login\" class=\"login button\" type=\"button\"\r\n" + 
-				"					value=\"Login\"></li>\r\n" + 
-				"				<li>\r\n" + 
-				"				<form method=\"get\" action=\"/logout\">\r\n" + 
-				"				<button class=\"login button\" type=\"submit\">Logout\r\n" + 
-				"				</form>	\r\n" + 
-				"				</li>\r\n" + 
-				"			</div>\r\n" + 
-				"		</nav>\r\n" + 
+		return "<!DOCTYPE html>" + 
+				"<html lang=\"en\">" + 
+				"<head>" + 
+				"<meta charset=\"UTF-8\">" + 
+				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" + 
+				"<title>NotEatYet</title>" + 
+				"<script src=\"/js/jquery-3.5.1.min.js\"></script>" + 
+				"<script src=\"/js/main2.js\"></script>" + 
+				"<link rel=\"stylesheet\" href=\"/css/style.css\">" + 
+				"</head>" + 
+				"<body>" + 
+				"	<header class=\"top-header\">" + 
+				"		<nav class=\"top-nav\">" + 
+				"			<li><input id=\"modal-registrati\" class=\"registrati button\"" + 
+				"					type=\"button\" value=\"Registrati\"></li>" + 
+				"			<!--LOGO-->" + 
+				"			<li class=\"logo\"><a href=\"/index.html\"><img class=\"img-logo\"" + 
+				"					src=\"https://i.ibb.co/RDLFZ4c/prova06.png\" alt=\"\"></a></li>" + 
+				"			<!--REGISTRA LOGIN-->" + 
+				"			<div class=\"top-reg-log\">" + 
+				"				<li><input id=\"modal-login\" class=\"login button\" type=\"button\"" + 
+				"					value=\"Login\"></li>" + 
+				"				<li>" + 
+				"				<form method=\"get\" action=\"/logout\">" + 
+				"				<button class=\"login button\" type=\"submit\">Logout" + 
+				"				</form>" + 
+				"				</li>" + 
+				"			</div>" + 
+				"		</nav>" + 
 				"	</header>" +
-				"<div class='marginLR'>"
-				+ "<h3>Accesso eseguito</h3><br>"
-				+ "<h4>Benvenuto in Not Eat Yet</h4><br>"
-				+ "<a href='/index.html'>Vai alla Home</a>"
-				+ "</div>"
-				+ "<!--FIXME-->\r\n" + 
-				"		<footer>\r\n" + 
-				"			<div class=\"torna-home marginLR\">\r\n" + 
-				"				<a href='/index.html'>&#9664; Torna alla Home</a>\r\n" + 
-				"			</div>\r\n" + 
-				"			<div class=\"clas footer-content\">\r\n" + 
-				"				<div class=\"footer-section about\"></div>\r\n" + 
-				"				<div class=\"footer-section links\"></div>\r\n" + 
-				"				<div class=\"footer-section contact\"></div>\r\n" + 
-				"		\r\n" + 
-				"			</div>\r\n" + 
-				"			<div class=\"footer-bottom\">© 2020 All Rights Reserved\r\n" + 
-				"				NotEatYet.com</div>\r\n" + 
-				"		</footer>\r\n" + 
-				"			<!--fine fixme-->\r\n" + 
-				"	</body>\r\n" + 
+				"<div class='marginLR'>" +
+				"<h3>Accesso eseguito</h3><br>" +
+				"<h4>Benvenuto in Not Eat Yet</h4><br>" +
+				"<a href='/index.html'>Vai alla Home</a>" +
+				"</div>" +
+				"		<footer>" + 
+				"			<div class=\"torna-home marginLR\">" + 
+				"				<a href='/index.html'>&#9664; Torna alla Home</a>" + 
+				"			</div>" + 
+				"			<div class=\"clas footer-content\">" + 
+				"				<div class=\"footer-section about\"></div>" + 
+				"				<div class=\"footer-section links\"></div>" + 
+				"				<div class=\"footer-section contact\"></div>" + 
+				"			</div>" + 
+				"			<div class=\"footer-bottom\">© 2020 All Rights Reserved" + 
+				"				NotEatYet.com</div>" + 
+				"		</footer>" + 
+				"	</body>" + 
 				"</html>";
 	}
 }
